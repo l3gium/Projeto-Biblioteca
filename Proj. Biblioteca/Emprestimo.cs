a6244dff9b785c100ea520f681b6b9dbdd5b1e73
Miguel Luizatto Alves <131935219+l3gium@users.noreply.github.com>
2024-11-03 23:00:33 -0300

    Add files via upload

---
+++ Proj. Biblioteca/Emprestimo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj.Biblioteca
+{
+    public class Emprestimo
+    {
+        private DateTime dtEmprestimo;
+        private DateTime? dtDevolucao;
+
+        public DateTime getDtEmprestimo()
+        {
+            return dtEmprestimo;
+        }
+
+        public void setDtEmprestimo(DateTime dtEmprestimo)
+        {
+            this.dtEmprestimo = dtEmprestimo;
+        }
+
+        public DateTime? getDtDevolucao()
+        {
+            return dtDevolucao;
+        }
+
+        public void setDtDevolucao(DateTime? dtDevolucao)
+        {
+            this.dtDevolucao = dtDevolucao;
+        }
+    }
+}
